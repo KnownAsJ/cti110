f34b7bc3a1ee50babe1325e65cc80f4d99057c1a
KnownAsJ <160173496+KnownAsJ@users.noreply.github.com>
2024-02-15 14:36:35 -0500

    Add files via upload

---
+++ P1LAB1_McRaeJames.py
@@ -0,0 +1,16 @@
+# James McRae
+# 2/15/24
+# P1LAB1
+# This program will display my name in a welcome message
+
+#Get the first name from the user and assign it to a variable
+
+fname= input ("Enter your first name: ")
+#print(fname)
+
+#Get the last name from the user and assign it to a variable
+
+lname= input ("Enter your last name: ")
+#print(lname)
+#print out the names and a welcome message.
+print("Hello,",fname,lname+'! Welcome to CTI-110')

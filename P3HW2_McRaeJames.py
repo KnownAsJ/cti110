8a10f17c7fb50c59a8ca66e032c3a6bd0168d9a0
KnownAsJ <160173496+KnownAsJ@users.noreply.github.com>
2024-03-28 13:38:33 -0400

    Add files via upload

--- P3HW2_McRaeJames.py
+++ P3HW2_McRaeJames.py
@@ -1,15 +1,15 @@
 ##James McRae
 ##3/26/24
 ##P3HW2
-##
+##Calculating the pay of an employee with or without overtime.
 employee=input("Enter employee's name: ")
 hrs = float(input("Enter number of hours worked: "))
 rate= float(input("Enter employee's pay rate: "))
 print('-'*37)
 print("Employee name:" + employee)
 
-print("Hours Worked")
-print("-"*100)
+print("Hours Worked   Pay Rate   OverTime   OverTime Pay   RegHour Pay   Gross Pay")
+print("-"*80)
 
 if hrs >40:
     reg_pay=rate*40
@@ -23,9 +23,6 @@ else:
  reg_pay=rate*hrs
  gross_pay= reg_pay + ovt_pay
 
-print(hrs)
-print(rate)
-print(ovt_hrs)
-print(ovt_pay)
-print(reg_pay)
-print(gross_pay)
+print(f'{hrs:<15.1f}' f'{rate:<10.1f}' f'{ovt_hrs:<12.1f}' f'{ovt_pay:<15.2f}' f'{reg_pay:<15.2f}' f'{gross_pay:<15.2f}')
+#Enter Employee's name, hrs worked, and work rate.
+#If hrs are over 40 then the overtime rate will be calculated.
